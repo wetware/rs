7f0d73a0490f8ea5dd32d829a7842d6493b5f6e1
mikelsr <mikelsr@pm.me>
2024-04-02 12:36:00 +0200

    Replace echo example with ping example

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,46 @@
-use std::{io, time::Duration};
+use std::{error::Error, time::Duration};
 
 use anyhow::Result;
-use futures::{AsyncReadExt, AsyncWriteExt, StreamExt};
-use libp2p::{identity, mdns, swarm::{NetworkBehaviour,SwarmEvent}, PeerId, Stream, StreamProtocol};
-use libp2p_stream as stream;
-use rand::RngCore;
-use tracing::level_filters::LevelFilter;
+use futures::StreamExt;
+use libp2p::{
+    identity, mdns, noise, ping,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux, PeerId,
+};
 use tracing_subscriber::EnvFilter;
-
-const ECHO_PROTOCOL: StreamProtocol = StreamProtocol::new("/echo");
 
 #[derive(NetworkBehaviour)]
 #[behaviour(to_swarm = "BehaviourEvent")]
 struct Behaviour {
     mdns: mdns::tokio::Behaviour,
-    stream: stream::Behaviour,
+    ping: ping::Behaviour,
 }
 
 #[derive(Debug)]
 enum BehaviourEvent {
     // Events emitted by the Mdns behaviour.
     Mdns(mdns::Event),
-    // Events emitted by the Stream behaviour.
-    Stream(()),
+    // Events emitted by the Ping behaviour.
+    Ping(ping::Event),
 }
 
 impl From<mdns::Event> for BehaviourEvent {
     fn from(event: mdns::Event) -> Self {
-        // TODO handle event
         BehaviourEvent::Mdns(event)
     }
 }
 
-impl From<()> for BehaviourEvent {
-    fn from(_: ()) -> Self {
-        // TODO handle event
-        BehaviourEvent::Stream(())
+impl From<ping::Event> for BehaviourEvent {
+    fn from(event: ping::Event) -> Self {
+        BehaviourEvent::Ping(event)
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::builder()
-                .with_default_directive(LevelFilter::INFO.into())
-                .from_env()?,
-        )
-        .init();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
 
     // Create a MDNS network behaviour.
     let id_keys = identity::Keypair::generate_ed25519();
@@ -55,151 +48,49 @@ async fn main() -> Result<()> {
     let mdns_behaviour = mdns::tokio::Behaviour::new(mdns::Config::default(), peer_id)?;
 
     // Create Stream behaviour.
-    let stream_behaviour = stream::Behaviour::new();
+    let ping_behaviour = ping::Behaviour::default();
 
     // Combine behaviours.
     let behaviour = Behaviour {
         mdns: mdns_behaviour,
-        stream: stream_behaviour,
+        ping: ping_behaviour,
     };
 
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
-        .with_quic()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
         .with_behaviour(|_| behaviour)?
-        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(10)))
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
         .build();
 
-    swarm.listen_on("/ip4/127.0.0.1/udp/0/quic-v1".parse()?)?;
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    let mut incoming_streams = swarm
-        .behaviour()
-        .stream
-        .new_control()
-        .accept(ECHO_PROTOCOL)
-        .unwrap();
-
-    // Deal with incoming streams.
-    // Spawning a dedicated task is just one way of doing this.
-    // libp2p doesn't care how you handle incoming streams but you _must_ handle them somehow.
-    // To mitigate DoS attacks, libp2p will internally drop incoming streams if your application cannot keep up processing them.
-    tokio::spawn(async move {
-        // This loop handles incoming streams _sequentially_ but that doesn't have to be the case.
-        // You can also spawn a dedicated task per stream if you want to.
-        // Be aware that this breaks backpressure though as spawning new tasks is equivalent to an unbounded buffer.
-        // Each task needs memory meaning an aggressive remote peer may force you OOM this way.
-
-        while let Some((peer, stream)) = incoming_streams.next().await {
-            match echo(stream).await {
-                Ok(n) => {
-                    tracing::info!(%peer, "Echoed {n} bytes!");
-                }
-                Err(e) => {
-                    tracing::warn!(%peer, "Echo failed: {e}");
-                    continue;
-                }
-            };
-        }
-    });
-
-    // In this demo application, the dialing peer initiates the protocol.
-    // if let Some(address) = maybe_address {
-    //     let Some(Protocol::P2p(peer_id)) = address.iter().last() else {
-    //         anyhow::bail!("Provided address does not end in `/p2p`");
-    //     };
-
-    //     swarm.dial(address)?;
-
-    //     tokio::spawn(connection_handler(
-    //         peer_id,
-    //         swarm.behaviour().stream.new_control(),
-    //     ));
-    // }
-
-    // Poll the swarm to make progress.
     loop {
-        let event = swarm.next().await.expect("never terminates");
-
-        match event {
-            SwarmEvent::NewListenAddr { address, .. } => {
-                let listen_address = address.with_p2p(*swarm.local_peer_id()).unwrap();
-                tracing::info!(%listen_address);
-            }
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
             SwarmEvent::Behaviour(BehaviourEvent::Mdns(event)) => {
-                // Handle mdns behaviour events.
-                tracing::trace!(?event);
+                print!("mdns: ");
+                println!("{event:?}");
+                match event {
+                    mdns::Event::Discovered(peers) => {
+                        for (_, addr) in peers {
+                            swarm.dial(addr)?;
+                        }
+                    }
+                    mdns::Event::Expired(_) => {}
+                }
             }
-            SwarmEvent::Behaviour(BehaviourEvent::Stream(())) => {
-                // Handle stream behaviour events.
-                tracing::trace!(?event);
+            SwarmEvent::Behaviour(BehaviourEvent::Ping(event)) => {
+                print!("ping: ");
+                println!("{event:?}")
             }
-            event => {
-             tracing::trace!(?event)}
-            }
+            _ => {}
         }
     }
-}
-
-/// A very simple, `async fn`-based connection handler for our custom echo protocol.
-async fn connection_handler(peer: PeerId, mut control: stream::Control) {
-    loop {
-        tokio::time::sleep(Duration::from_secs(1)).await; // Wait a second between echos.
-
-        let stream = match control.open_stream(peer, ECHO_PROTOCOL).await {
-            Ok(stream) => stream,
-            Err(error @ stream::OpenStreamError::UnsupportedProtocol(_)) => {
-                tracing::info!(%peer, %error);
-                return;
-            }
-            Err(error) => {
-                // Other errors may be temporary.
-                // In production, something like an exponential backoff / circuit-breaker may be more appropriate.
-                tracing::debug!(%peer, %error);
-                continue;
-            }
-        };
-
-        if let Err(e) = send(stream).await {
-            tracing::warn!(%peer, "Echo protocol failed: {e}");
-            continue;
-        }
-
-        tracing::info!(%peer, "Echo complete!")
-    }
-}
-
-async fn echo(mut stream: Stream) -> io::Result<usize> {
-    let mut total = 0;
-
-    let mut buf = [0u8; 100];
-
-    loop {
-        let read = stream.read(&mut buf).await?;
-        if read == 0 {
-            return Ok(total);
-        }
-
-        total += read;
-        stream.write_all(&buf[..read]).await?;
-    }
-}
-
-async fn send(mut stream: Stream) -> io::Result<()> {
-    let num_bytes = rand::random::<usize>() % 1000;
-
-    let mut bytes = vec![0; num_bytes];
-    rand::thread_rng().fill_bytes(&mut bytes);
-
-    stream.write_all(&bytes).await?;
-
-    let mut buf = vec![0; num_bytes];
-    stream.read_exact(&mut buf).await?;
-
-    if bytes != buf {
-        return Err(io::Error::new(io::ErrorKind::Other, "incorrect echo"));
-    }
-
-    stream.close().await?;
-
-    Ok(())
 }
