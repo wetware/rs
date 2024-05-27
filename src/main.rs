8fa74400b01cbcde86a7a0f5320535c36a07fbe4
mikelsr <mikelsr@pm.me>
2024-05-27 15:14:51 +0200

    Create Cfg trait and its default implementation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::{env, error::Error, time::Duration};
+use std::{error::Error, time::Duration};
 
 use anyhow::Result;
-use libp2p::{identify, identity, kad, mdns, noise, ping, swarm, tcp, yamux, PeerId};
+use libp2p::{identify, kad, mdns, noise, ping, swarm, tcp, yamux};
 use tracing_subscriber::EnvFilter;
 
 use ww_net;
 
-fn is_kad_client() -> bool {
-    let args: Vec<String> = env::args().collect();
-    return args.iter().any(|arg| arg == "--kad-client");
-}
+pub mod cfg;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Use the default configuration.
+    let config: &dyn cfg::Cfg = &cfg::DefaultCfg::new();
+
     // Start configuring a `fmt` subscriber
     let subscriber = tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
@@ -26,20 +26,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
     // Create a MDNS network behaviour.
-    let id_keys = identity::Keypair::generate_ed25519();
-    let peer_id = PeerId::from(id_keys.public());
-    let mdns_behaviour = mdns::tokio::Behaviour::new(mdns::Config::default(), peer_id)?;
+    let mdns_behaviour = mdns::tokio::Behaviour::new(mdns::Config::default(), config.peer_id())?;
 
     // Create Stream behaviour.
     let ping_behaviour = ping::Behaviour::default();
 
-    // Create Kademlia behaviour.
+    // Create Kademlia and Identify behaviours.
     let kad_cfg = kad::Config::default();
-    let store = kad::store::MemoryStore::new(id_keys.public().to_peer_id());
-    let kad_behaviour = kad::Behaviour::with_config(id_keys.public().to_peer_id(), store, kad_cfg);
+    let kad_store = kad::store::MemoryStore::new(config.id_keys().public().to_peer_id());
+    let kad_behaviour =
+        kad::Behaviour::with_config(config.id_keys().public().to_peer_id(), kad_store, kad_cfg);
     let identify_behaviour = identify::Behaviour::new(identify::Config::new(
-        "/test/1.0.0.".to_owned(),
-        id_keys.public(),
+        config.identify_protocol(),
+        config.id_keys().public(),
     ));
 
     // Combine behaviours.
@@ -50,7 +49,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         identify: identify_behaviour,
     };
 
-    let raw_swarm = libp2p::SwarmBuilder::with_existing_identity(id_keys)
+    let raw_swarm = libp2p::SwarmBuilder::with_existing_identity(config.id_keys())
         .with_tokio()
         .with_tcp(
             tcp::Config::default(),
@@ -61,19 +60,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
         .build();
 
+    // Wrap the swarm in our custom type to overwrite its behaviour and event management.
     let mut swarm = ww_net::DefaultSwarm(raw_swarm);
 
-    // Configure Kademlia mode, defaults to server.
-    let kad_mode = if is_kad_client() {
-        kad::Mode::Client
-    } else {
-        kad::Mode::Server
-    };
-    swarm.behaviour_mut().kad.set_mode(Some(kad_mode));
+    // Set the Kademlia mode.
+    swarm.behaviour_mut().kad.set_mode(Some(config.kad_mode()));
 
-    // Tell the swarm to listen on all interfaces and a random, OS-assigned
-    // port.
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned port.
+    swarm.listen_on(config.listen_addr().parse()?)?;
 
     loop {
         match swarm.select_next_some().await {
@@ -82,11 +76,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 // using our PeerID as the key.
                 tracing::info!("listening on {address:?}")
             }
-
             swarm::SwarmEvent::Behaviour(ww_net::DefaultBehaviourEvent::Mdns(event)) => {
                 ww_net::net::default_mdns_handler(&mut swarm, event);
             }
-
             swarm::SwarmEvent::Behaviour(ww_net::DefaultBehaviourEvent::Ping(event)) => {
                 tracing::info!("got PING event: {event:?}");
             }
